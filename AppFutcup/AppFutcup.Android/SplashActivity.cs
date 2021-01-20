cca10641b32c0806ec65f29a77b6bc067c10d8b0
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-20 15:47:06 -0300

    Adicionar arquivos de projeto.

---
+++ AppFutcup/AppFutcup.Android/SplashActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+using System.Threading.Tasks;
+
+namespace AppFutcup.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        public override void OnBackPressed() { }
+
+        async void SimulateStartup()
+        {
+            Log.Debug(TAG, "Performing some startup work that takes a bit of times.");
+            await Task.Delay(5000);
+            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
---
+++ AppFutcup/AppFutcup/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppFutcup
+{
+    //by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private const string View = "https://www.ligafutcup.esportes.co/";
+        public MainPage()
+        {
+            InitializeComponent();
+            webview1.Source = View;
+        }
+    }
+}
